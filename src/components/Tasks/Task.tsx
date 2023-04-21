fe1d3b357f534e66236568e1f4bccb75820f3289
Amanuel Genene <amanuelgenene11@gmail.com>
2023-04-21 18:21:23 +0300

    Show task status history

--- src/components/Tasks/Task.tsx
+++ src/components/Tasks/Task.tsx
@@ -6,6 +6,12 @@ import {
   Button,
   Typography,
   IconButton,
+  Modal,
+  Paper,
+  Step,
+  StepContent,
+  StepLabel,
+  Stepper,
 } from "@mui/material";
 import { FC, useState } from "react";
 import { Edit } from "@mui/icons-material";
@@ -66,6 +72,38 @@ const Task: FC<{ task: TaskType }> = ({ task }) => {
           </Link>
         </IconButton>
       </CardActions>
+      <Modal
+        open={open}
+        onClose={() => setOpen(false)}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box
+          sx={{
+            maxWidth: 400,
+            position: "absolute" as "absolute",
+            top: "50%",
+            left: "50%",
+            transform: "translate(-50%, -50%)",
+            width: 400,
+            bgcolor: "background.paper",
+            border: "2px solid #000",
+            boxShadow: 24,
+            p: 4,
+          }}
+        >
+          <Typography id="modal-modal-title" variant="h4" marginBottom={2}>
+            History
+          </Typography>
+          <Stepper activeStep={task.history.length} orientation="vertical">
+            {[...task.history].reverse().map((step, index) => (
+              <Step key={step + index}>
+                <StepLabel>{step}</StepLabel>
+              </Step>
+            ))}
+          </Stepper>
+        </Box>
+      </Modal>
     </Card>
   );
 };
